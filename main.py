f7897ea23b6c78042d14e1d040e1d9c25e964cad
JosiasAurel <ndjosiasaurel@gmail.com>
2022-10-25 22:42:00 +0100

    it works

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from telethon.sync import TelegramClient
 import json
+import requests
 
+HN_BASE_URL = "https://hacker-news.firebaseio.com/v0"
+
+# time to play around with Hacker News weird API
+def fetch_posts():
+    top_stories = requests.get(f"{HN_BASE_URL}/topstories.json").json()
+
+    latest_articles = []
+
+    for count, id_ in enumerate(top_stories):
+        if count == 20: break
+        article = requests.get(f"{HN_BASE_URL}/item/{id_}.json").json()
+        latest_articles.append(article)
+
+    return latest_articles
 
 with open("config.json", "r") as config_json:
     json_content = json.loads(config_json.read())
@@ -9,6 +24,15 @@ api_id = json_content.get("api_id")
 api_hash = json_content.get("api_hash")
 
 with TelegramClient("name", api_id, api_hash) as client:
-    client.send_message("me", "I love you Josias")
-
-    client.run_until_disconnected()
+    posts = fetch_posts()
+    # time to send all those messages
+    client.send_message("me", "Good morning, Master. \nHere is your daily dose of Hacker News")
+    for post in posts:
+        message = f"""
+            {post.get("title")}            
+            Posted by {post.get("by")}
+            More -> {post.get("url")}
+        """
+        client.send_message("me", message)
+    client.send_message("me", "That's it for today.")
+    client.disconnect()
