8bec14ef7dc8e873adbf79c1d96236cb48bfa540
JosiasAurel <ndjosiasaurel@gmail.com>
2022-10-25 23:02:31 +0100

    add fastapi and format code

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from telethon.sync import TelegramClient
 import json
 import requests
+from fastapi import FastAPI
+
+app = FastAPI()
 
 HN_BASE_URL = "https://hacker-news.firebaseio.com/v0"
 
@@ -11,11 +14,13 @@ def fetch_posts():
     latest_articles = []
 
     for count, id_ in enumerate(top_stories):
-        if count == 20: break
+        if count == 20:
+            break
         article = requests.get(f"{HN_BASE_URL}/item/{id_}.json").json()
         latest_articles.append(article)
 
     return latest_articles
+
 
 with open("config.json", "r") as config_json:
     json_content = json.loads(config_json.read())
@@ -23,16 +28,21 @@ with open("config.json", "r") as config_json:
 api_id = json_content.get("api_id")
 api_hash = json_content.get("api_hash")
 
-with TelegramClient("name", api_id, api_hash) as client:
-    posts = fetch_posts()
-    # time to send all those messages
-    client.send_message("me", "Good morning, Master. \nHere is your daily dose of Hacker News")
-    for post in posts:
-        message = f"""
-            {post.get("title")}            
-            Posted by {post.get("by")}
-            More -> {post.get("url")}
-        """
-        client.send_message("me", message)
-    client.send_message("me", "That's it for today.")
-    client.disconnect()
+
+@app.get("/")
+def _root():
+    with TelegramClient("name", api_id, api_hash) as client:
+        posts = fetch_posts()
+        # time to send all those messages
+        client.send_message(
+            "me", "Good morning, Master. \nHere is your daily dose of Hacker News"
+        )
+        for post in posts:
+            message = f"""
+				{post.get("title")}            
+				Posted by {post.get("by")}
+				More -> {post.get("url")}
+				"""
+            client.send_message("me", message)
+            client.send_message("me", "That's it for today.")
+            client.disconnect()
