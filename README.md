648041d9c53b2f5cea872714a94dccbdddc7e9e4
JosiasAurel <ndjosiasaurel@gmail.com>
2022-10-25 22:01:50 +0100

    setup telethon and send test message

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Telegram News Bot
+
+A bot that will fetch news from HackerNews and send it to me on Telegram every morning.
+
--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from telethon.sync import TelegramClient
+import json
 
-print("Hello World");
+
+with open("config.json", "r") as config_json:
+    json_content = json.loads(config_json.read())
+
+api_id = json_content.get("api_id")
+api_hash = json_content.get("api_hash")
+
+with TelegramClient("name", api_id, api_hash) as client:
+    client.send_message("me", "I love you Josias")
+
+    client.run_until_disconnected()
